96cd20cfbfeb94f33d0148eb9da720b45adf72ef
unknown <gausulislam5@gmail.com>
2023-10-13 11:44:43 +0600

    jobs json loader and display

--- src/components/Feature/Feature.jsx
+++ src/components/Feature/Feature.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import './Feature.css'
 
-const Feature = ({feature}) => {
+const Feature = ({ feature }) => {
+    const { id, logo, job_title, company_name, remote_or_onsite, location, job_type, salary } = feature
     return (
-        <div>
-            <h1>hemonto: {feature.length}</h1>
+        <div className='feature-container'>
+            <img src={logo} alt="" />
+            <h4>{job_title}</h4>
+            <p>{company_name}</p>
+            <div className='btn-remote-type'>
+                <button className='btn-remote'>{remote_or_onsite}</button>
+                <button className='btn-remote'>{job_type}</button>
+            </div>
+            <div className='location'>
+                <p>{location}</p>
+                <p>{salary}</p>
+            </div>
+
+            <button className='btn-view-details'>View Details</button>
         </div>
     );
 };
--- src/components/Features/Features.jsx
+++ src/components/Features/Features.jsx
@@ -1,26 +1,34 @@
 import React, { useEffect, useState } from 'react';
 import Feature from '../Feature/Feature';
+import './Features.css'
 
 const Features = () => {
-    const [features,setFeatures]=useState([]);
-    useEffect(()=>{
-        const loadData=async()=>{
-            const res=await fetch('jobs.json');
-            const data=await res.json();
+    const [features, setFeatures] = useState([]);
+    useEffect(() => {
+        const loadData = async () => {
+            const res = await fetch('jobs.json');
+            const data = await res.json();
             setFeatures(data)
         }
         loadData();
-    },[])
+    }, [])
     return (
         <div>
-            <h1>Featured Jobs</h1>
-            <p>Explore thousands of job opportunities with all the information you need. Its your future</p>
-            {
-                features.map(feature=><Feature
-                key={feature.id}
-                feature={feature}
-                ></Feature>)
-            }
+            <h1 className='features-job-title'>Featured Jobs</h1>
+            <p className='features-job-description'>Explore thousands of job opportunities with all the information you need. Its your future</p>
+            <div className='features-container'>
+                {
+                    features.map(feature => <Feature
+                        key={feature.id}
+                        feature={feature}
+                    ></Feature>)
+                }
+            </div>
+            <div className='text-center'>
+                <button className='btn-see-more'>See More</button>
+            </div>
+
+
         </div>
     );
 };
