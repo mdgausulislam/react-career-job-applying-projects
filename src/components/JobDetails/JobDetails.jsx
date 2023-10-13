8dea9a8f5a25cf30786bb694ef1660a15ac7784a
unknown <gausulislam5@gmail.com>
2023-10-14 00:45:01 +0600

    job description add

--- src/components/JobDetails/JobDetails.jsx
+++ src/components/JobDetails/JobDetails.jsx
@@ -1,16 +1,27 @@
 import React from 'react';
 import { useLoaderData, useParams } from 'react-router-dom';
+import './JobDetails.css'
 
 const JobDetails = () => {
     const jobs = useLoaderData();
-    const {id} = useParams();
+    const { id } = useParams();
     const idInt = parseInt(id);
     const job = jobs.find(job => job.id === idInt);
-    console.log(job); 
+    console.log(job);
     return (
-        <div>
-            
-           
+        <div className='job-all-details'>
+            <h1 className='job-type'>Job Details</h1>
+
+            <div className='job-type-container'>
+                <div className='job-type-description-container'>
+                    <p className='job-description'><span className='job-description-title'>Job Description:</span>{job.job_description}</p>
+
+                </div>
+                <div className='job-details-container'>
+                    <h1>oder details</h1>
+                </div>
+
+            </div>
         </div>
     );
 };
